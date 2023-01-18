98337453cd67c361b3364b72d726bcf8e95fec23
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-01-18 14:54:16 +0800

    add one more

--- apps/aptos/config/constants/ifo.ts
+++ apps/aptos/config/constants/ifo.ts
@@ -4,6 +4,29 @@ import { Ifo } from './types'
 
 export const ifos: Ifo[] = [
   {
+    id: 'USDC',
+    cIFO: false,
+    address: `${IFO_POOL_STORE_TAG}<${mainnetTokens.dai.address}, ${mainnetTokens.usdc.address}, ${getIFOUID(1)}>`,
+    isActive: true,
+    name: 'USDC',
+    poolUnlimited: {
+      saleAmount: '9 USDC',
+      raiseAmount: '$123,456',
+      cakeToBurn: '$0',
+      distributionRatio: 0.7,
+    },
+    currency: mainnetTokens.dai,
+    token: mainnetTokens.usdc,
+    releaseTime: 0,
+    campaignId: '1',
+    articleUrl: '',
+    tokenOfferingPrice: 0.00004939,
+    version: 3.2,
+    twitterUrl: '',
+    description: 'USDC description',
+    vestingTitle: '$USDC - USDC vesting title.',
+  },
+  {
     id: 'DAI',
     cIFO: false,
     address: `${IFO_POOL_STORE_TAG}<${mainnetTokens.busd.address}, ${mainnetTokens.dai.address}, ${getIFOUID(1)}>`,
